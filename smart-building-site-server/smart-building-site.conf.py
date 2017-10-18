f77ad1257ffe3651a761acaca218ab6d5ba28b2a
quanpower <quanpower@gmail.com>
2017-10-18 19:04:14 +0800

    * 1.add smart-building-site.conf for supervisor

--- smart-building-site-server/smart-building-site.conf.py
+++ smart-building-site-server/smart-building-site.conf.py
@@ -1,21 +1,21 @@
 ; [program:xx]是被管理的进程配置参数，xx是进程的名称
-[program:smart-building-site-api]
-command=/root/ENV/smart-building-site/bin/python /root/smart-building-site/smart-building-site-server/api.py
-environment=PATH="/root/ENV/smart-building-site/bin"
-directory=/root/smart-building-site/smart-building-site-server
-autostart=true       ; 在supervisord启动的时候也自动启动
-startsecs=10         ; 启动10秒后没有异常退出，就表示进程正常启动了，默认为1秒
-autorestart=true     ; 程序退出后自动重启,可选值：[unexpected,true,false]，默认为unexpected，表示进程意外杀死后才重启
-startretries=3       ; 启动失败自动重试次数，默认是3
-user=root          ; 用哪个用户启动进程，默认是root
-priority=666         ; 进程启动优先级，默认999，值小的优先启动
-redirect_stderr=true ; 把stderr重定向到stdout，默认false
-stdout_logfile_maxbytes=20MB  ; stdout 日志文件大小，默认50MB
-stdout_logfile_backups = 20   ; stdout 日志文件备份数，默认是10
+;[program:smart-building-site-api]
+;command=/root/ENV/smart-building-site/bin/python /root/smart-building-site/smart-building-site-server/api.py
+;environment=PATH="/root/ENV/smart-building-site/bin"
+;directory=/root/smart-building-site/smart-building-site-server
+;autostart=true       ; 在supervisord启动的时候也自动启动
+;startsecs=10         ; 启动10秒后没有异常退出，就表示进程正常启动了，默认为1秒
+;autorestart=true     ; 程序退出后自动重启,可选值：[unexpected,true,false]，默认为unexpected，表示进程意外杀死后才重启
+;startretries=3       ; 启动失败自动重试次数，默认是3
+;user=root          ; 用哪个用户启动进程，默认是root
+;priority=666         ; 进程启动优先级，默认999，值小的优先启动
+;redirect_stderr=true ; 把stderr重定向到stdout，默认false
+;stdout_logfile_maxbytes=20MB  ; stdout 日志文件大小，默认50MB
+;stdout_logfile_backups = 20   ; stdout 日志文件备份数，默认是10
 ; stdout 日志文件，需要注意当指定目录不存在时无法正常启动，所以需要手动创建目录（supervisord 会自动创建日志文件）
-stdout_logfile=/root/smart-building-site/smart-building-site-server/log/supervisor_smart-building-site-api.out
-stopasgroup=false     ;默认为false,进程被杀死时，是否向这个进程组发送stop信号，包括子进程
-killasgroup=false     ;默认为false，向进程组发送kill信号，包括子进程
+;stdout_logfile=/root/smart-building-site/smart-building-site-server/log/supervisor_smart-building-site-api.out
+;stopasgroup=false     ;默认为false,进程被杀死时，是否向这个进程组发送stop信号，包括子进程
+;killasgroup=false     ;默认为false，向进程组发送kill信号，包括子进程
 
 
 [program:smart-building-site-mqtt-sub]
@@ -42,7 +42,7 @@ killasgroup=false     ;默认为false，向进程组发送kill信号，包括子
 [program:smart-building-site-fab]
 
 command=/root/ENV/smart-building-site/bin/fabmanager run --port 6688; 程序启动命令
-environment=PATH="/root/ENV/smart-iiot/bin"
+environment=PATH="/root/ENV/smart-building-site/bin"
 directory=/root/smart-building-site/smart-building-site-server
 
 autostart=true       ; 在supervisord启动的时候也自动启动
