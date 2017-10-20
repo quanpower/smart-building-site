d42bc0fa07d8fbca970fa6f79ec1728883c1bef6
quanpower <quanpower@gmail.com>
2017-10-20 11:07:17 +0800

    * 1.edit api.py,run.py

--- smart-building-site-server/run.py
+++ smart-building-site-server/run.py
@@ -8,25 +8,28 @@ import datetime
 
 gatewayAddr='1'
 
-nodeAddr = '1'
+nodeAddrs = ['110', '112', '114']
 
 startTime = datetime.datetime.strptime('2017-10-20 00:00:00', "%Y-%m-%d %H:%M:%S")
 endTime = datetime.datetime.strptime('2017-10-20 11:00:00', "%Y-%m-%d %H:%M:%S")
 
+for i in range(len(nodeAddrs)):
+    nodeAddr = nodeAddrs[i]
+    print('--------nodeAddr-----------')
+    print(nodeAddr)
+    temp_records = db.session.query(ConcTemp.temp1, ConcTemp.temp2, ConcTemp.temp3, ConcTemp.temp4, ConcTemp.temp5, ConcTemp.temp6, ConcTemp.datetime).filter(and_(ConcGateway.gateway_addr == gatewayAddr, ConcNode.node_addr == nodeAddr, ConcTemp.datetime.between(startTime, endTime))).order_by(ConcTemp.datetime.desc()).all()
 
-temp_records = db.session.query(ConcTemp.temp1, ConcTemp.temp2, ConcTemp.temp3, ConcTemp.temp4, ConcTemp.temp5, ConcTemp.temp6, ConcTemp.datetime).filter(and_(ConcGateway.gateway_addr == gatewayAddr, ConcNode.node_addr == nodeAddr, ConcTemp.datetime.between(startTime, endTime))).order_by(ConcTemp.datetime.desc()).all()
+    temp_log = []
+    for i in xrange(len(temp_records)):
+        temp_log.append({"key": i, "time": temp_records[i][6].strftime("%Y-%m-%d %H:%M:%S"), "Temp1": temp_records[i][0], "Temp2": temp_records[i][1], "Temp3": temp_records[i][2], "Temp4": temp_records[i][3], "Temp5": temp_records[i][4], "Temp6": temp_records[i][5]})
 
-temp_log = []
-for i in xrange(len(temp_records)):
-    temp_log.append({"key": i, "time": temp_records[i][6].strftime("%Y-%m-%d %H:%M:%S"), "Temp1": temp_records[i][0], "Temp2": temp_records[i][1], "Temp3": temp_records[i][2], "Temp4": temp_records[i][3], "Temp5": temp_records[i][4], "Temp6": temp_records[i][5]})
+    temps_reverse = temp_log[::-1]
 
-temps_reverse = temp_log[::-1]
+    print('---------temp_records-----------')
+    print(temp_records)
 
-print('---------temp_records-----------')
-print(temp_records)
-
-print('---------temps_reverse-----------')
-print(temps_reverse)
+    print('---------temps_reverse-----------')
+    print(temps_reverse)
 
 
 
