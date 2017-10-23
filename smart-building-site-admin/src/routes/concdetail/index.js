aa1ce21e6a20932160ad5eb4bb832e84a0ce89d8
quanpower <quanpower@gmail.com>
2017-10-23 19:10:45 +0800

    * 1.edit concrete,add dispatch

--- smart-building-site-admin/src/routes/concdetail/index.js
+++ smart-building-site-admin/src/routes/concdetail/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
-import { Row, Col, Card, Select } from 'antd'
+import { Row, Col, Card, Select, message } from 'antd'
 import { color } from 'utils'
 import { Loader } from 'components'
 import {  ConcRealtimeTemp, ConcTemps, ConcTempRecord } from './components'
