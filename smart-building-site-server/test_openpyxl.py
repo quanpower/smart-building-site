f943f09508afd23967393e5421c4a3203b2fecd2
quanpower <quanpower@gmail.com>
2017-11-09 20:01:12 +0800

    edit test_openpyxl.py

--- smart-building-site-server/test_openpyxl.py
+++ smart-building-site-server/test_openpyxl.py
@@ -3,25 +3,24 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
 
-wb = load_workbook('sample.xlsx')  #加载一个工作簿
-print wb.get_sheet_names()
+wb = load_workbook('concrete_template.xlsx')  #加载一个工作簿
+print(wb.get_sheet_names())
 
-wb = Workbook()
+# wb = Workbook()
 #创建一个工作簿
-ws1 = wb.active
+ws = wb.active
 #至少建立一个工作表
-ws = wb.create_sheet("11",0)
-ws1.title = "22"
 #设置表的名字
 ws.sheet_properties.tabColor = "2072BA"
 #改变表选项卡的颜色
-ws["F8"] = 38.1
+ws["I8"] = 38.1
+print(ws["I8"].value)
 
 ws.cell(row=8, column=9, value=29.1)
 ws.cell(row=9, column=9, value=48.9)
 ws.cell(row=10, column=9, value=40.3)
 
 
-print(ws["A2"].value)
+print(ws["I8"].value)
 
-wb.save("sample1.xlsx")
+wb.save("sample1-1.xlsx")
