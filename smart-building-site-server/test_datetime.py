13b70004f796081786e1e53e4bd3d2872b973012
quanpower <quanpower@gmail.com>
2017-11-10 23:01:42 +0800

    edit test_datetime

--- smart-building-site-server/test_datetime.py
+++ smart-building-site-server/test_datetime.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*- 
+
+from openpyxl import load_workbook
+from openpyxl import Workbook
+
 import datetime
 
 datetime_now = datetime.datetime.now()
@@ -24,3 +29,28 @@ for i in range(len(datetime_list)):
 
 print('---------datetime_str_list-----------')
 print(datetime_str_list)
+
+
+
+
+wb = load_workbook('concrete_template.xlsx')  #加载一个工作簿
+print(wb.get_sheet_names())
+
+# wb = Workbook()
+#创建一个工作簿
+ws = wb.active
+#至少建立一个工作表
+#设置表的名字
+ws.sheet_properties.tabColor = "2072BA"
+#改变表选项卡的颜色
+ws["I8"] = 38.1
+print(ws["I8"].value)
+
+ws.cell(row=8, column=9, value=29.1)
+ws.cell(row=9, column=9, value=48.9)
+ws.cell(row=10, column=9, value=40.3)
+
+
+print(ws["I8"].value)
+
+wb.save("sample1-1.xlsx")
